2adc1e50f258f7f8b516366f1d0e9f4b7f8657ca
KentoTokura <tkento1985@icloud.com>
2023-12-18 18:09:02 +0900

    add button.c

--- main.c
+++ main.c
@@ -22,54 +22,38 @@ int cnt;
 int state = INIT, pos = 0;
 
 /* interrupt_handler() is called every 100msec */
-void interrupt_handler()
-{
-  if (state == INIT)
-  {
-  }
-  else if (state == OPENING)
-  {
+void interrupt_handler() {
+  if (state == INIT) {
+  } else if (state == OPENING) {
     cnt = 0;
-  }
-  else if (state == PLAY)
-  {
-
+  } else if (state == PLAY) {
     /* Display a ball */
     pos = (cnt < 16) ? cnt : 31 - cnt;
     show_ball(pos);
-    if (++cnt >= 32)
-    {
+    if (++cnt >= 32) {
       cnt = 0;
     }
-  }
-  else if (state == ENDING)
-  {
+  } else if (state == ENDING) {
     end_display();
   }
 }
-void main()
-{
-  while (1)
-  {
-    if (state == INIT)
-    {
+
+void main() {
+  while (1) {
+    if (state == INIT) {
       start_display();
-      while (!ioc_check_0()) // ボタンが押されるのを待つ(未完成）
+      while (!ioc_check_0())  // ボタンが押されるのを待つ(未完成）
       {
         ;
       }
       state = PLAY;
-    }
-    else if (state == PLAY)
-    {
+    } else if (state == PLAY) {
       cnt = 0;
       play();
       state = ENDING;
-    }
-    else if (state == ENDING)
-    {
+    } else if (state == ENDING) {
       end_display();
-      while (~btn_check_my()) // ボタンが押されるのを待つ(未完成）
+      while (~btn_check_my())  // ボタンが押されるのを待つ(未完成）
       {
       }
       state = OPENING;
@@ -77,117 +61,64 @@ void main()
   }
 }
 
-void start_display()
-{
+void start_display() {
   lcd_cmd(0x80);
   lcd_str("Game Start");
   lcd_cmd(0xc0);
   lcd_str("Press Button");
 }
 
-void end_display()
-{
+void end_display() {
   lcd_cmd(0x80);
   lcd_str("Game End");
   lcd_cmd(0xc0);
   lcd_str("Press Button");
 }
 
-void play()
-{
-  while (1)
-  {
+void play() {
+  while (1) {
     /* Button0 is pushed when the ball is in the left edge */
 
-    if (cnt == 28 && ioc_check_0())
-    {
+    if (cnt == 28 && ioc_check_0()) {
       cnt = 3;
-    }
-    else if (cnt == 29 && ioc_check_0())
-    {
+    } else if (cnt == 29 && ioc_check_0()) {
       cnt = 2;
-    }
-    else if (cnt == 30 && ioc_check_0())
-    {
+    } else if (cnt == 30 && ioc_check_0()) {
       cnt = 1;
     }
 
-    else if (cnt == 12 && ioc_check_0())
-    {
+    else if (cnt == 12 && ioc_check_0()) {
       cnt = 19;
-    }
-    else if (cnt == 13 && ioc_check_0())
-    {
+    } else if (cnt == 13 && ioc_check_0()) {
       cnt = 18;
-    }
-    else if (cnt == 14 && ioc_check_0())
-    {
+    } else if (cnt == 14 && ioc_check_0()) {
       cnt = 17;
     }
 
-    if (pos == 0 && btn_check_0())
-    {
+    if (pos == 0 && btn_check_0()) {
       led_blink(); /* Blink LEDs when hit */
                    /* Button3 is pushed when the ball is in the right edge */
-    }
-    else if (pos == 15 && btn_check_3())
-    {
+    } else if (pos == 15 && btn_check_3()) {
       led_blink(); /* Blink LEDs when hit */
-    }
-    else if (btn_check_1())
-    {
+    } else if (btn_check_1()) {
       break; /* Stop the game */
     }
   }
 }
-void show_ball(int pos)
-{
+void show_ball(int pos) {
   lcd_cmd(0x01);       /* Clear display */
   lcd_cmd(0x80 + pos); /* Set cursor position */
   lcd_data('a');
-}
-/*
- * Switch functions
- */
-int btn_check_my() // 何らかのボタンが押されたかどうか(未完成）
-{
-}
-int btn_check_0()
-{
-  volatile int *sw_ptr = (int *)0xff04;
-  ;
-  return (*sw_ptr & 0x10) ? 1 : 0;
-}
-int btn_check_1()
-{
-  volatile int *sw_ptr = (int *)0xff04;
-  ;
-  return (*sw_ptr & 0x20) ? 1 : 0;
-}
-int btn_check_3()
-{
-  volatile int *sw_ptr = (int *)0xff04;
-  ;
-  return (*sw_ptr & 0x80) ? 1 : 0;
-}
-
-int ioc_check_0()
-{
-  volatile int *sw_ptr = (int *)0xff14;
-  ;
-  return (*sw_ptr & 0x1) ? 1 : 0;
 }
 
 /*
  * LED functions
  */
-void led_set(int data)
-{
+void led_set(int data) {
   volatile int *led_ptr = (int *)0xff08;
   *led_ptr = data;
 }
-void led_blink()
-{
+void led_blink() {
   int i;
   led_set(0xf); /* Turn on */
   for (i = 0; i < 300000; i++)
@@ -203,14 +134,12 @@ void led_blink()
 /*
  * LCD functions
  */
-void lcd_wait(int n)
-{
+void lcd_wait(int n) {
   int i;
   for (i = 0; i < n; i++)
     ;
 }
-void lcd_cmd(unsigned char cmd)
-{
+void lcd_cmd(unsigned char cmd) {
   /* E, RS, RW, DB[7:0] */
   volatile int *lcd_ptr = (int *)0xff0c;
   *lcd_ptr = (0x000000ff & cmd) | 0x00000000; /* E=0,RS=0,RW=0 */
@@ -220,8 +149,7 @@ void lcd_cmd(unsigned char cmd)
   *lcd_ptr = (0x000000ff & cmd) | 0x00000000; /* E=0,RS=0,RW=0 */
   lcd_wait(11389);
 }
-void lcd_data(unsigned char data)
-{
+void lcd_data(unsigned char data) {
   /* E, RS, RW, DB[7:0] */
   volatile int *lcd_ptr = (int *)0xff0c;
   *lcd_ptr = (0x000000ff & data) | 0x00000200; /* E=0,RS=1,RW=0 */
@@ -231,8 +159,7 @@ void lcd_data(unsigned char data)
   *lcd_ptr = (0x000000ff & data) | 0x00000200; /* E=0,RS=1,RW=0 */
   lcd_wait(278);
 }
-void lcd_init()
-{
+void lcd_init() {
   lcd_wait(104167);
   lcd_cmd(0x38); /* 8-bit, 2-line mode */
   lcd_cmd(0x06); /* Cursor auto increment */
@@ -241,8 +168,6 @@ void lcd_init()
   lcd_wait(10417);
 }
 
-void lcd_str(unsigned char *str)
-{
-  while (*str != 0x00)
-    lcd_data(*str++);
+void lcd_str(unsigned char *str) {
+  while (*str != 0x00) lcd_data(*str++);
 }
---
+++ mips/soft/button.c
@@ -0,0 +1,33 @@
+/*
+ * Switch functions
+ */
+
+#define IO_A_ADDR 0xff1a  // between display and speaker
+// #define IO_B_ADDR 0xff10  // speaker
+#define IO_C_ADDR 0xff14  // under the display
+#define BTN_ADDR 0xff04
+
+int btn_check_num(int button_num) {
+  volatile int *btn_ptr = (int *)BTN_ADDR;
+  switch (button_num) {
+    case 0:
+      return (*btn_ptr & 0x10) ? 1 : 0;
+    case 1:
+      return (*btn_ptr & 0x20) ? 1 : 0;
+    case 2:
+      return (*btn_ptr & 0x40) ? 1 : 0;
+    case 3:
+      return (*btn_ptr & 0x80) ? 1 : 0;
+    default:
+      return 0;
+  }
+}
+
+int btn_check_a() {
+  volatile int *sw_ptr = (int *)IO_A_ADDR;
+  return (*sw_ptr & 0x1) ? 1 : 0;
+}
+int btn_check_c() {
+  volatile int *sw_ptr = (int *)IO_C_ADDR;
+  return (*sw_ptr & 0x1) ? 1 : 0;
+}
