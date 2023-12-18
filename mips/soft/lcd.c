e3ddb4a4a94bfc352a05a51d169502733fe88ade
KentoTokura <tkento1985@icloud.com>
2023-12-18 17:30:53 +0900

    add animation

--- mips/soft/lcd.c
+++ mips/soft/lcd.c
@@ -12,29 +12,27 @@ void lcd_char(unsigned char data);
 void lcd_str(unsigned char *str);
 
 // LCD アニメーション関数
-void demo()
-{
-  int i, j;
-  unsigned char data[DISPLAY_ROW][DISPLAY_COL] = {
-      " Hello, World.      ",
-      " Hello, World.      ",
-      " Hello, World.      ",
-      " Hello, World.      "};
-
+void lcd_demo_animation() {
   lcd_init();
-  lcd_update(data);
+  unsigned char data[DISPLAY_ROW][DISPLAY_COL] = {
+      " Hello, World.      ", " Hello, World.      ", " Hello, World.      ",
+      " Hello, World.      "};
+  int i, j;
+  for (i = 0; i < DISPLAY_ROW; i++) {
+    for (j = 0; j < DISPLAY_COL; j++) {
+      lcd_update(data);
+      data[i][j] = ' ';
+    }
+    lcd_wait(3000 * 1000);
+  }
 }
 
-// 以下は仮の実装です。実際の環境に合わせて LCD 制御関数を実装してください。
-/* condition */
-void lcd_wait(int n)
-{
+void lcd_wait(int n) {
   int i;
   for (i = 0; i < n; i++)
     ;
 }
-void lcd_cmd(unsigned char cmd)
-{
+void lcd_cmd(unsigned char cmd) {
   /* E, RS, RW, DB[7:0] */
   volatile int *lcd_ptr = (int *)0xff0c;
   *lcd_ptr = (0x000000ff & cmd) | 0x00000000; /* E=0,RS=0,RW=0 */
@@ -44,8 +42,7 @@ void lcd_cmd(unsigned char cmd)
   *lcd_ptr = (0x000000ff & cmd) | 0x00000000; /* E=0,RS=0,RW=0 */
   lcd_wait(11389);
 }
-void lcd_data(unsigned char data)
-{
+void lcd_data(unsigned char data) {
   /* E, RS, RW, DB[7:0] */
   volatile int *lcd_ptr = (int *)0xff0c;
   *lcd_ptr = (0x000000ff & data) | 0x00000200; /* E=0,RS=1,RW=0 */
@@ -55,8 +52,7 @@ void lcd_data(unsigned char data)
   *lcd_ptr = (0x000000ff & data) | 0x00000200; /* E=0,RS=1,RW=0 */
   lcd_wait(278);
 }
-void lcd_init()
-{
+void lcd_init() {
   lcd_wait(104167);
   lcd_cmd(0x38); /* 8-bit, 2-line mode */
   lcd_cmd(0x06); /* Cursor auto increment */
@@ -64,32 +60,27 @@ void lcd_init()
   lcd_cmd(0x01); /* Clear display */
   lcd_wait(10417);
 }
-void lcd_str(unsigned char *str)
-{
-  while (*str != 0x00)
-    lcd_data(*str++);
+void lcd_str(unsigned char *str) {
+  while (*str != 0x00) lcd_data(*str++);
 }
 
-void lcd_update(unsigned char data[DISPLAY_ROW][DISPLAY_COL])
-{
+void lcd_update(unsigned char data[DISPLAY_ROW][DISPLAY_COL]) {
   int i;
-  int pos; // address
-  for (i = 0; i < DISPLAY_ROW; i++)
-  {
-    switch (i)
-    {
-    case 0:
-      pos = 0x80;
-      break;
-    case 1:
-      pos = 0xc0;
-      break;
-    case 2:
-      pos = 0x94;
-      break;
-    case 3:
-      pos = 0xd4;
-      break;
+  int pos;  // address
+  for (i = 0; i < DISPLAY_ROW; i++) {
+    switch (i) {
+      case 0:
+        pos = 0x80;
+        break;
+      case 1:
+        pos = 0xc0;
+        break;
+      case 2:
+        pos = 0x94;
+        break;
+      case 3:
+        pos = 0xd4;
+        break;
     }
     lcd_cmd(pos);
     lcd_str(data[i]);
