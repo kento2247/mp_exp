0b998ce96d07978522c14d580b84cd13ce125e76
kento2247 <tkento1985@icloud.com>
2023-12-19 15:02:05 +0900

    fix handler bug

--- mips/soft/handler_func.c
+++ mips/soft/handler_func.c
@@ -2,32 +2,42 @@
 
 // 定数の宣言
 #define MAX_UINT 4294967295
-#define HANDLER_INTERVAL 10  // 10msec
+#define HANDLER_INTERVAL 10 // 10msec
 
 // グローバル変数
 unsigned int handler_cnt = 0;
 
-void handler_counter() {
+void handler_counter()
+{
   // ハンドラカウンタを更新する
   handler_cnt++;
-  if (handler_cnt >= MAX_UINT) handler_cnt = 0;
+  if (handler_cnt >= MAX_UINT)
+    handler_cnt = 0;
 }
 
-void handler_sleep(unsigned int msec) {
+void handler_sleep(unsigned int msec)
+{
   // 10msec単位でsleepする関数
   unsigned int start = handler_cnt;
   unsigned int add_time = msec / HANDLER_INTERVAL;
-  unsigned int laxucity = MAX_UINT - start;  // オーバーフローまでの猶予
+  unsigned int laxucity = MAX_UINT - start; // オーバーフローまでの猶予
   unsigned int end;
-  if (laxucity < add_time) {
+  int is_enable_interrupt = enable_interrupt;
+  if (enable_interrupt == 0)
+    enable_interrupt = 1;
+  if (laxucity < add_time)
+  {
     unsigned int difference = add_time - laxucity;
     while (handler_cnt < MAX_UINT)
       ;
     end = difference;
-  } else
+  }
+  else
     end = start + add_time;
-  while (handler_cnt < end) {
+  while (handler_cnt < end)
+  {
     // nop
     ;
   }
+  enable_interrupt = is_enable_interrupt;
 }
