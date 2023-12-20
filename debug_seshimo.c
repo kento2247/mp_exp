6769cf0cedaf44bae15bedeed94089eb06e70481
sryo200 <sryo2713@gmail.com>
2023-12-20 17:54:58 +0900

    ボールの往復とHPの制御、表示

--- debug_seshimo.c
+++ debug_seshimo.c
@@ -20,6 +20,7 @@ void sleep(unsigned int msec);
 int pos;
 int cnt;
 int just_flag = 0;
+int play_stop_flag = 0;
 unsigned int hp_playerA = 5;
 unsigned int hp_playerB = 5;
 
@@ -31,15 +32,16 @@ unsigned char intToChar(int num)
 
 void show_ball(int pos)
 {
-    lcd_cmd(0x01); /* Clear display */
-    lcd_cmd(0xc0 + pos);
+    lcd_cmd(0x01);       /* Clear display */
+    lcd_cmd(0xc0 + pos); // 2列目をボールが往復する
     lcd_data('o');
     lcd_cmd(0xd4);
     lcd_str("HP");
     lcd_cmd(0xd4 + 3);
     lcd_data(intToChar(hp_playerA));
+    // lcd_digit3(intToChar(hp_playerA));
     lcd_cmd(0xd4 + 15);
-    lcd_str("HP")
+    lcd_str("HP");
     lcd_cmd(0xd4 + 19);
     lcd_data(intToChar(hp_playerB));
 }
@@ -57,7 +59,7 @@ void interrupt_handler()
         hp_playerA = 5;
         hp_playerB = 5;
     }
-    if (game_state == 1)
+    if (game_state == 1 && play_stop_flag == 0)
     {
         pos = (cnt < 200) ? cnt / 10 : 39 - cnt / 10;
         show_ball(pos);
@@ -65,7 +67,7 @@ void interrupt_handler()
         {
             if (++cnt >= 400)
             {
-                cnt = 0;
+                cnt = 400; // 点数をとられてしまうから0に戻す必要はない
             }
         }
         else if (just_flag == 1)
@@ -73,9 +75,21 @@ void interrupt_handler()
             cnt = cnt + 2;
             if (cnt >= 400)
             {
-                cnt = 0;
+                cnt = 400;
             }
         }
+    }
+    if (game_state == 1 && play_stop_flag == 1)
+    {
+        cnt = 20;
+        pos = (cnt < 200) ? cnt / 10 : 39 - cnt / 10;
+        show_ball(pos);
+    }
+    if (game_state == 1 && play_stop_flag == 2)
+    {
+        cnt = 220;
+        pos = (cnt < 200) ? cnt / 10 : 39 - cnt / 10;
+        show_ball(pos);
     }
 }
 
@@ -90,40 +104,78 @@ void setup()
 
 void play()
 {
-    while (1)
+    while (hp_playerA > 0 && hp_playerB > 0)
     {
         /* Button0 is pushed when the ball is in the left edge */
+        if (play_stop_flag == 0)
+        {
+            if ((cnt >= 360 && cnt <= 369) && btn_check_a())
+            {
+                cnt = 30;
+                just_flag = 0;
+            }
+            else if ((cnt >= 370 && cnt <= 379) && btn_check_a())
+            {
+                cnt = 20;
+                just_flag = 1;
+            }
+            else if ((cnt >= 380 && cnt <= 389) && btn_check_a())
+            {
+                cnt = 10;
+                just_flag = 0;
+            }
+            else if (cnt >= 400)
+            {
+                // hp_playerA -= 1; // Aが失点
+                play_stop_flag = 1;
+                break;
+            }
 
-        if ((cnt >= 360 && cnt <= 369) && btn_check_a())
-        {
-            cnt = 30;
-            just_flag = 0;
+            else if ((cnt >= 160 && cnt <= 169) && btn_check_a())
+            {
+                cnt = 230;
+                just_flag = 0;
+            }
+            else if ((cnt >= 170 && cnt <= 179) && btn_check_a())
+            {
+                cnt = 220;
+                just_flag = 1;
+            }
+            else if ((cnt >= 180 && cnt <= 189) && btn_check_a())
+            {
+                cnt = 210;
+                just_flag = 0;
+            }
+            else if (cnt >= 200 && cnt <= 210)
+            {
+                // hp_playerB -= 1; // Bが失点
+                play_stop_flag = 2;
+                break;
+            }
         }
-        else if ((cnt >= 370 && cnt <= 379) && btn_check_a())
+        else if (play_stop_flag == 1)
         {
-            cnt = 20;
-            just_flag = 1;
+            hp_playerA += 1;
+            while (!btn_check_a())
+            {
+            }
+            while (!btn_check_a())
+            {
+            }
+            // hp_playerA += 1;
+            play_stop_flag = 0;
         }
-        else if ((cnt >= 380 && cnt <= 389) && btn_check_a())
+        else if (play_stop_flag == 2)
         {
-            cnt = 10;
-            just_flag = 0;
-        }
-
-        else if ((cnt >= 160 && cnt <= 169) && btn_check_a())
-        {
-            cnt = 230;
-            just_flag = 0;
-        }
-        else if ((cnt >= 170 && cnt <= 179) && btn_check_a())
-        {
-            cnt = 220;
-            just_flag = 1;
-        }
-        else if ((cnt >= 180 && cnt <= 189) && btn_check_a())
-        {
-            cnt = 210;
-            just_flag = 0;
+            hp_playerB -= 1;
+            while (!btn_check_a())
+            {
+            }
+            while (!btn_check_a())
+            {
+            }
+            // hp_playerB -= 1;
+            play_stop_flag = 0;
         }
     }
 }
