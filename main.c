cc2f0fabd96bd38673d975f12510502d3ae3ece6
KentoTokura <tkento1985@icloud.com>
2024-01-02 22:29:40 +0900

    fix flow

--- main.c
+++ main.c
@@ -1,7 +1,3 @@
-// タイトル：リズムテニスゲーム
-// アピールポイント
-// 乱数は使っていない
-// 5班
 // デバイスの初期化を行う。必ずcrt0.cを最初にincludeする
 #include "crt0.c"
 
@@ -21,77 +17,55 @@ void main();
 
 // 割り込みハンドラ
 // interrupt_handler() is called every 10msec (HANDLER_INTERVAL msec)
-void interrupt_handler()
-{
-  handler_counter(); // ハンドラカウンタを更新する
-  if (enable_interrupt == 0)
-    return; // 割り込みが無効なら何もしない
+void interrupt_handler() {
+  handler_counter();                  // ハンドラカウンタを更新する
+  if (enable_interrupt == 0) return;  // 割り込みが無効なら何もしない
 
-  switch (game_state)
-  {
-  case 0:
-    game_init();
-    int i;
-    for (i = 0; i < 10; i++)
-    {
-      tone_play(4);
-    }
-    game_opening();
-    game_state = 1;
-    break;
-  case 1:
-    if (life[0] == 0 || life[1] == 0)
-      game_state = 2; // gameを終了し、次の状態へ遷移
-    else
-      game_play();
-    break;
-  case 2:
-    game_ending((life[0] == 0) ? 1 : 0);
-    while (1)
-    {
-      btn_states_update();
-      if (btn_states[0])
-      {
-        game_state = 3;
-        break;
-      }
-      else if (btn_states[1])
-      {
-        game_state = 0;
-        break;
-      }
-    }
-    break;
-  case 3:
-    lcd_clear();
-    break;
+  switch (game_state) {
+    case -1:
+      game_state = 0;
+    case 0:
+      game_init();
+      // tone_demo();
+      game_opening();
+      game_state = 1;
+      break;
+    case 1:
+      if (life[0] == 0 || life[1] == 0)
+        game_state = 2;  // gameを終了し、次の状態へ遷移
+      else
+        game_play();
+      break;
+    case 2:
+      game_ending((life[0] == 0) ? 1 : 0);
+      // btn_wait_any();
+      // game_state = 3;
+      break;
+    default:
+      lcd_clear();
+      break;
   }
 }
 
 // 初期設定
 
-void setup()
-{
+void setup() {
   lcd_init();
   // lcd_demo_animation();
   // tone_demo();
-  enable_interrupt = 1; // 割り込みを有効にする（開始）
+  enable_interrupt = 1;  // 割り込みを有効にする（開始）
 }
 // メインループ
-void loop()
-{
-  btn_states_update(); // ボタンの状態を更新する(button.c)
+void loop() {
+  btn_states_update();  // ボタンの状態を更新する(button.c)
 }
 
 // メイン関数
-void main()
-{
+void main() {
   setup();
-  while (1)
-  {
+  while (1) {
     loop();
-    if (game_state == 3)
-    {
+    if (game_state == 3) {
       break;
     }
   }
--- mips/soft/game.c
+++ mips/soft/game.c
@@ -11,29 +11,27 @@ void game_opening();
 void game_ending(int winner);
 void game_init();
 void game_play();
-void game_show_ball();
+void game_show_ball(int ball_index);
 void game_judge();
 unsigned char intToChar(int num);
 
 // ゲームの状態を表す変数
-int game_state = 0; // 0: opening, 1: playing, 2: ending
+int game_state = 0;  // 0: opening, 1: playing, 2: ending
 int life[2] = {5, 5};
-int virturl_index;      // ゲーム判定をするための仮想的なx軸の長さ:
-                        // 0~200（実際にはlen=20マス）
-int move_direction = 1; // ボールの移動方向。1: 増加, -1: 減少
-int just_flag = 0;      // 0: 通常, 1: just。球速を変化させる条件に使用
+int virturl_index;  // ゲーム判定をするための仮想的なx軸の長さ:
+                    // 0~200（実際にはlen=20マス）
+int move_direction = 1;  // ボールの移動方向。1: 増加, -1: 減少
+int just_flag = 0;  // 0: 通常, 1: just。球速を変化させる条件に使用
 int play_stop_flag =
-    0; // deadline missを検知したら1にする。btmを押してゲーム再開
+    0;  // deadline missを検知したら1にする。btmを押してゲーム再開
 int renda_A_flag = 50;
 int renda_B_flag = 50;
 int serve_flag = 1;
 int critical_flag = 2;
 
-void game_init()
-{
+void game_init() {
   int i = 0;
-  for (i = 0; i < 2; i++)
-    life[i] = 5;
+  for (i = 0; i < 2; i++) life[i] = 5;
   game_state = 0;
   virturl_index = 10;
   move_direction = 1;
@@ -41,16 +39,9 @@ void game_init()
   renda_B_flag = 50;
   serve_flag = 1;
   critical_flag = 2;
-  /*
-  for (i = 0; i < 6; i++)
-  {
-    btn_states[i] = 0;
-  }
-  */
 }
 
-void game_opening()
-{
+void game_opening() {
   lcd_clear();
 
   lcd_cmd(0xc0 + 2);
@@ -58,13 +49,12 @@ void game_opening()
   lcd_cmd(0x94 + 5);
   lcd_str("Game start!");
 
-  btn_wait_any(); // buttonが何かしら押されるまで待つ
+  btn_wait_any();  // buttonが何かしら押されるまで待つ
 
-  lcd_clear(); // 画面をクリアする
+  lcd_clear();  // 画面をクリアする
 }
 
-void game_ending(int winner)
-{
+void game_ending(int winner) {
   lcd_cmd(0x01);
   lcd_cmd(0x80);
 
@@ -80,12 +70,9 @@ void game_ending(int winner)
   */
   lcd_update(data);
   lcd_cmd(0xc0 + 17);
-  if (winner == 0)
-  {
+  if (winner == 0) {
     lcd_data('A');
-  }
-  else if (winner == 1)
-  {
+  } else if (winner == 1) {
     lcd_data('B');
   }
   /*
@@ -117,10 +104,9 @@ void game_ending(int winner)
 */
 }
 
-void game_show_ball(int ball_index)
-{
-  lcd_cmd(0x01);              /* Clear display */
-  lcd_cmd(0xc0 + ball_index); // 2列目をボールが往復する
+void game_show_ball(int ball_index) {
+  lcd_cmd(0x01);               /* Clear display */
+  lcd_cmd(0xc0 + ball_index);  // 2列目をボールが往復する
   lcd_data('o');
   lcd_cmd(0xd4);
   lcd_str("HP");
@@ -151,114 +137,95 @@ void game_show_ball(int ball_index)
   lcd_data(0x07);
 }
 
-void game_play()
-{
+void game_play() {
   /* Button0 is pushed when the ball is in the left edge */
-  if (play_stop_flag == 0) // ボールを動かすとき
+  if (play_stop_flag == 0)  // ボールを動かすとき
   {
     renda_A_flag = renda_A_flag + 1;
     renda_B_flag = renda_B_flag + 1;
     game_judge();
-  }
-  else if (play_stop_flag == 1) // Aが点を失ったとき
+  } else if (play_stop_flag == 1)  // Aが点を失ったとき
   {
     renda_A_flag = 50;
     renda_B_flag = 50;
-    just_flag = 0;
     btn_wait_A();
+    just_flag = 0;
     play_stop_flag = 0;
     serve_flag = 1;
-  }
-  else if (play_stop_flag == 2) // Bが点を失ったとき
+  } else if (play_stop_flag == 2)  // Bが点を失ったとき
   {
     renda_A_flag = 100;
     renda_B_flag = 100;
-    just_flag = 0;
     btn_wait_C();
+    just_flag = 0;
     play_stop_flag = 0;
     serve_flag = 1;
   }
 }
 
-void game_judge()
-{
+void game_judge() {
   int i;
-  if (btn_get_state(4)) // ボタンAが押されたとき
+  if (btn_get_state(4))  // ボタンAが押されたとき
   {
-    if ((virturl_index > 170 && virturl_index <= 180 && renda_B_flag >= 50))
-    {
-      for (i = 0; i < 10; i++)
-      {
-        tone_play(4);
+    if (renda_B_flag >= 50) {
+      int tone_type = 0;  // 鳴らす音の種類
+      if ((virturl_index > 170 && virturl_index <= 180)) {
+        tone_type = 4;
+        just_flag = 0;
+        for (i = 0; i < 10; i++) {
+          tone_play(tone_type);
+        }
+        move_direction = -1;
+      } else if ((virturl_index > 180 && virturl_index <= 190)) {
+        if (serve_flag) {
+          tone_type = 0;
+          just_flag = 0;
+        } else {
+          tone_type = 1;
+          just_flag = 1;
+          critical_flag++;
+        }
+        move_direction = -1;
+        for (i = 0; i < 10; i++) {
+          tone_play(tone_type);
+        }
+      } else if ((virturl_index > 190 && virturl_index <= 200)) {
+        tone_type = 4;
+        just_flag = 0;
+        move_direction = -1;
+        for (i = 0; i < 10; i++) {
+          tone_play(tone_type);
+        }
       }
-      just_flag = 0;
-      move_direction = -1;
-    }
-    else if ((virturl_index > 180 && virturl_index <= 190 && renda_B_flag >= 50 && serve_flag == 0))
-    {
-      for (i = 0; i < 10; i++)
-      {
-        tone_play(1);
-      }
-      just_flag = 1;
-      move_direction = -1;
-      critical_flag++;
-    }
-    else if ((virturl_index > 180 && virturl_index <= 190 && renda_B_flag >= 50 && serve_flag == 1))
-    {
-      for (i = 0; i < 10; i++)
-      {
-        tone_play(4);
-      }
-      just_flag = 0;
-      move_direction = -1;
-    }
-    else if ((virturl_index > 190 && virturl_index <= 200 && renda_B_flag >= 50))
-    {
-      for (i = 0; i < 10; i++)
-      {
-        tone_play(4);
-      }
-      just_flag = 0;
-      move_direction = -1;
     }
     renda_B_flag = 0;
     serve_flag = 0;
-  }
-  else if (btn_get_state(5)) // ボタンCが押されたとき
+  } else if (btn_get_state(5))  // ボタンCが押されたとき
   {
-    if ((virturl_index >= 20 && virturl_index < 30 && renda_A_flag >= 50))
-    {
-      for (i = 0; i < 10; i++)
-      {
+    if ((virturl_index >= 20 && virturl_index < 30 && renda_A_flag >= 50)) {
+      for (i = 0; i < 10; i++) {
         tone_play(4);
       }
       just_flag = 0;
       move_direction = 1;
-    }
-    else if ((virturl_index >= 10 && virturl_index < 20 && renda_A_flag >= 50 && serve_flag == 0))
-    {
-      for (i = 0; i < 10; i++)
-      {
+    } else if ((virturl_index >= 10 && virturl_index < 20 &&
+                renda_A_flag >= 50 && serve_flag == 0)) {
+      for (i = 0; i < 10; i++) {
         tone_play(8);
       }
       just_flag = 1;
       move_direction = 1;
       critical_flag++;
-    }
-    else if ((virturl_index >= 10 && virturl_index < 20 && renda_A_flag >= 50 && serve_flag == 1))
-    {
-      for (i = 0; i < 10; i++)
-      {
+    } else if ((virturl_index >= 10 && virturl_index < 20 &&
+                renda_A_flag >= 50 && serve_flag == 1)) {
+      for (i = 0; i < 10; i++) {
         tone_play(4);
       }
       just_flag = 0;
       move_direction = 1;
-    }
-    else if ((virturl_index >= 0 && virturl_index < 10 && renda_A_flag >= 50))
-    {
-      for (i = 0; i < 10; i++)
-      {
+    } else if ((virturl_index >= 0 && virturl_index < 10 &&
+                renda_A_flag >= 50)) {
+      for (i = 0; i < 10; i++) {
         tone_play(4);
       }
       just_flag = 0;
@@ -268,39 +235,32 @@ void game_judge()
     serve_flag = 0;
   }
 
-  if (!play_stop_flag)
-  {
-    if (just_flag == 0 || serve_flag == 1) // 通常モードかサーブ
+  if (!play_stop_flag) {
+    if (just_flag == 0 || serve_flag == 1)  // 通常モードかサーブ
     {
       critical_flag = 2;
       virturl_index = virturl_index + move_direction * critical_flag;
-    }
-    else
-    {
+    } else {
       // 倍速モード
       virturl_index = virturl_index + move_direction * critical_flag;
     }
   }
 
-  if (virturl_index > 200)
-  {
-    move_direction = -1; // ボールの移動方向を左へ
+  if (virturl_index > 200) {
+    move_direction = -1;  // ボールの移動方向を左へ
     virturl_index = 189;
-    life[1]--; // Cはdeadline miss
+    life[1]--;  // Cはdeadline miss
     play_stop_flag = 1;
-  }
-  else if (virturl_index < 0)
-  {
-    move_direction = 1; // ボールの移動方向を右へ
+  } else if (virturl_index < 0) {
+    move_direction = 1;  // ボールの移動方向を右へ
     virturl_index = 11;
-    life[0]--; // Aはdeadline miss
+    life[0]--;  // Aはdeadline miss
     play_stop_flag = 2;
   }
   game_show_ball(virturl_index / 10);
 }
 
-unsigned char intToChar(int num)
-{
+unsigned char intToChar(int num) {
   unsigned char res = '0' + num;
   return res;
 }
